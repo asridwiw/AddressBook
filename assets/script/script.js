b26144111bcb950536d765c025df6eda46381007
asridwiw <asridwiwahyuu@gmail.com>
2025-11-19 11:58:24 +0700

    add index.html

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+const contacts = [
+  {
+    id: 1,
+    fullname: "Asri Dwi Wahyuningrum",
+    phone: "6289611448239",
+    email: "asridwiwningrum@gmail.com",
+    location: "Bogor",
+  },
+  {
+    id: 2,
+    fullname: "Kwon Hoshi",
+    phone: "625712345110",
+    email: "hoshikwon17@gmail.com",
+    location: "Bogor",
+  },
+];
+
+console.log(contacts);
